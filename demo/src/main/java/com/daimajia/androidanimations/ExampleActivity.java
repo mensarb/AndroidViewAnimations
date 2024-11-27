6b8b77e82c434816b4534e602b1547dc0c3a01e3
dkoller <dominik.koller@myworld.com>
2024-11-27 09:44:31 +0100

    dependencies updated

--- demo/src/main/java/com/daimajia/androidanimations/ExampleActivity.java
+++ demo/src/main/java/com/daimajia/androidanimations/ExampleActivity.java
@@ -9,37 +9,30 @@ import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
 
 public class ExampleActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.example);
 
-        final TextView t = (TextView) findViewById(R.id.notice);
+        final TextView t = findViewById(R.id.notice);
         t.setText("Please input your Email and Password");
 
-        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        findViewById(R.id.submit).setOnClickListener(v -> {
+            YoYo.with(Techniques.Tada)
+                    .duration(700)
+                    .playOn(findViewById(R.id.edit_area));
 
-                YoYo.with(Techniques.Tada)
-                        .duration(700)
-                        .playOn(findViewById(R.id.edit_area));
-
-                t.setText("Wrong password!");
-            }
+            t.setText("Wrong password!");
         });
 
-        final TextView t2 = (TextView) findViewById(R.id.notice2);
+        final TextView t2 = findViewById(R.id.notice2);
         t2.setText("Please input your Email and Password");
 
-        findViewById(R.id.submit2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        findViewById(R.id.submit2).setOnClickListener(v -> {
+            YoYo.with(Techniques.Shake).playOn(findViewById(R.id.edit_area2));
 
-                YoYo.with(Techniques.Shake).playOn(findViewById(R.id.edit_area2));
-
-                t2.setText("Wrong password!");
-            }
+            t2.setText("Wrong password!");
         });
     }
 }
